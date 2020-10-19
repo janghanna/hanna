efd3c99afc36527b5985854a3c7685f5de90156b
janghanna <67949278+janghanna@users.noreply.github.com>
2020-10-19 17:59:57 +0900

    hailli

--- js/cart.js
+++ js/cart.js
@@ -1,16 +1,4 @@
 $(function () {
-    // $('.cark_table input[name=choice]').click(function(){
-    //     var chk = $(this).attr('checked');
-    //     if(chk) $('.cart_table input[name=choice]').prop('checked, true');
-    //     else $('.cart_table input[name=choice]').prop('checked',false);
-    // });
     
-    $("#allCheck").click(function(){ //전체선택 클릭
-        if($("#allCheck").prop("checked")){ 
-            $("input[type=checkbox]").prop("checked",true);
-        }else{
-            $("input[type=checkbox]").prop("ckecked",false);
-        }
-    })    
 });
 // window.addEventListener('DOMContentLoaded', init);
---
+++ js/checkbox.js
@@ -0,0 +1,21 @@
+$(function () {
+    //체크박스
+    $("#allCheck").click(function () { //전체선택 클릭
+        if ($("#allCheck").prop("checked")) {
+            //전체선택이 체크면
+            $("input[name=choice]").prop("checked", true); //다 체크 ㅇㅇ
+        } else { //아니면 체크ㄴㄴ
+            $("input[name=choice]").prop("checked", false);
+        }
+    });
+    //수량 count_price의 숫자가 바뀌면
+    //수량에따른 금액 item_price 가 바뀐다.
+    //총 주문금액 total_price 수량에따른금액 item_price의 총 합
+    
+    //
+    $('form').on('change', function(){
+       var  
+    });
+    
+});
+// window.addEventListener('DOMContentLoaded', init);
